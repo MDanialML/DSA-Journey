d3b09b8c7fdad404054a6cc2d8c65ae2c186b199
Muhammad Danial <m.danial465@gmail.com>
2026-05-07 23:01:02 +0500

    Count number of sub arrays whose sum is equal to k.

---
+++ Day30P2.java
@@ -0,0 +1,36 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class Day30P2{
+
+    public static int countSubarrays(int[] arr, int k){
+        if(arr.length == 0) return 0;
+        Map<Integer, Integer> prefixSums = new HashMap<>();
+        int result = 0;
+        int currSum = 0;
+        for(int i =0; i < arr.length; i++){
+            currSum += arr[i];
+
+            if(currSum == k){
+                result++;
+            }
+
+            if(prefixSums.containsKey(currSum - k)){
+                result += prefixSums.get(currSum - k);
+            }
+
+            prefixSums.put(currSum, prefixSums.getOrDefault(currSum, 0)+1);
+        }
+        return result;
+    }
+
+
+    //driver function
+    public static void main(String[] args){
+        int[] arr = {10, 2, -2, -20, 10};
+        int k = -10;
+        System.out.println(countSubarrays(arr, k));
+
+    }
+}
+
